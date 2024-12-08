d0ec4819913cd7e539cc127c12586a3ae77216ec
Xploit010 <edwinxploit9@gmail.com>
2024-12-08 16:15:52 -0500

    cambio

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST || 'localhost',
-    port: process.env.MYSQL_PORT || 3306,
-    user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || "",
-    database: process.env.MYSQL_DATABASE || "creditos"
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE, 
 })
 
 connection.connect(function (error) {
